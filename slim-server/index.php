f647a81799bf0418ce967fc3c3fdad5172431485
Till Schander <tillschander@gmx.de>
2020-04-06 14:10:49 +0200

    Example solution

--- slim-server/index.php
+++ slim-server/index.php
@@ -10,25 +10,74 @@ require __DIR__ . '/vendor/autoload.php';
 $app = AppFactory::create();
 $app->add(new TrailingSlash(false)); // remove trailing slashes
 
+$mongoClient = new MongoDB\Client('mongodb://localhost:27017');
+$cardsCollection = $mongoClient->selectCollection('flashcards-react-jerry', 'cards');
+
 // GET /
-$app->get('/', function (Request $request, Response $response, array $args) {
-    // TODO serve the html for the react app instead of "Hello world!"
-    $response->getBody()->write("Hello world!");
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write(file_get_contents('../client/build/index.html'));
+
     return $response;
 });
 
 // GET /cards
+$app->get('/cards', function (Request $request, Response $response, $args) use ($cardsCollection) {
+    $allCards = $cardsCollection->find()->toArray();
+    foreach ($allCards as $card) {
+        $card['_id'] = (string) $card['_id'];
+    }
+    $response->getBody()->write(json_encode($allCards));
+
+    return $response->withHeader('Content-Type', 'application/json');
+});
 
 // GET /cards/{id}
+$app->get('/cards/{id}', function (Request $request, Response $response, $args) use ($cardsCollection) {
+    $card = $cardsCollection->findOne(['_id' => new MongoDB\BSON\ObjectID($args['id'])]);
+    $card['_id'] = (string) $card['_id'];
+    $response->getBody()->write(json_encode($card));
+
+    return $response->withHeader('Content-Type', 'application/json');
+});
 
 // POST /cards
+$app->post('/cards', function (Request $request, Response $response, $args) use ($cardsCollection) {
+    $body = json_decode($request->getBody(), true);
+    $result = $cardsCollection->insertOne($body);
+    $card = $cardsCollection->findOne(['_id' => $result->getInsertedId()]);
+    $card['_id'] = (string) $card['_id'];
+    $response->getBody()->write(json_encode($card));
+
+    return $response->withStatus(201)->withHeader('Content-Type', 'application/json');
+});
 
 // PATCH /cards/{id}
+$app->patch('/cards/{id}', function (Request $request, Response $response, $args) use ($cardsCollection) {
+    $body = json_decode($request->getBody(), true);
+    $cardsCollection->updateOne(
+        ['_id' => new MongoDB\BSON\ObjectID($args['id'])],
+        ['$set' => $body]
+    );
+    $card = $cardsCollection->findOne(['_id' => new MongoDB\BSON\ObjectID($args['id'])]);
+    $card['_id'] = (string) $card['_id'];
+    $response->getBody()->write(json_encode($card));
+
+    return $response->withHeader('Content-Type', 'application/json');
+});
 
 // DELETE /cards/all
+$app->delete('/cards/all', function (Request $request, Response $response, $args) use ($cardsCollection) {
+    $cardsCollection->drop();
+
+    return $response;
+});
 
 // DELETE /cards/{id}
+$app->delete('/cards/{id}', function (Request $request, Response $response, $args) use ($cardsCollection) {
+    $cardsCollection->deleteOne(['_id' => new MongoDB\BSON\ObjectID($args['id'])]);
 
+    return $response;
+});
 
 $app->addErrorMiddleware(true, false, false);
 $app->run();
